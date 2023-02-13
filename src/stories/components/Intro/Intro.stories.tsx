46fd1414290fcb00f354d06381c00c52c3fceb18
Kinzume <81969626+Kinzume@users.noreply.github.com>
2023-02-13 16:34:12 +0000

    first commit

---
+++ src/stories/components/Intro/Intro.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Intro from "./Intro";
+
+export default {
+  title: "Portfolio/Intro",
+  component: Intro,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as ComponentMeta<typeof Intro>;
+
+const Template: ComponentStory<typeof Intro> = () => <Intro />;
+
+export const Primary = Template.bind({});
